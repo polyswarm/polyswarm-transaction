b3af5fa2b683a1952ca78cad5ecab836d1bba71e
Alan Justino <alan.justino@yahoo.com.br>
2020-03-30 20:20:05 -0300

    DepositTransactions + tests

--- src/polyswarmtransaction/nectar.py
+++ src/polyswarmtransaction/nectar.py
@@ -6,3 +6,8 @@ from polyswarmtransaction.transaction import Transaction
 @dataclasses.dataclass
 class WithdrawalTransaction(Transaction):
     amount: str
+
+
+@dataclasses.dataclass
+class DepositTransaction(Transaction):
+    amount: str
--- tests/test_nectar.py
+++ tests/test_nectar.py
@@ -5,7 +5,20 @@ from eth_keys.datatypes import PrivateKey
 from web3 import Web3
 
 from polyswarmtransaction.transaction import SignedTransaction
-from polyswarmtransaction.nectar import WithdrawalTransaction
+from polyswarmtransaction.nectar import WithdrawalTransaction, DepositTransaction
+
+
+def test_recover_deposit_when_computed(ethereum_accounts):
+    data = {
+        'name': 'polyswarmtransaction.nectar:DepositTransaction',
+        'from': '0x3f17f1962B36e491b30A40b2405849e597Ba5FB5',
+        'data': {
+            'amount': '2000000000000000000'
+        }
+    }
+    transaction = DepositTransaction('2000000000000000000')
+    signed = transaction.sign(ethereum_accounts[0].key)
+    assert signed.signature == PrivateKey(ethereum_accounts[0].key).sign_msg_hash(Web3.keccak(text=json.dumps(data)))
 
 
 def test_recover_withdrawal_when_computed(ethereum_accounts):
@@ -19,6 +32,22 @@ def test_recover_withdrawal_when_computed(ethereum_accounts):
     transaction = WithdrawalTransaction('2000000000000000000')
     signed = transaction.sign(ethereum_accounts[0].key)
     assert signed.signature == PrivateKey(ethereum_accounts[0].key).sign_msg_hash(Web3.keccak(text=json.dumps(data)))
+
+
+def test_sign_deposit_transaction(ethereum_accounts):
+    signature = ('0x59af24d65251f562dd8be5126917911aa1a660c8f5ce223f48e505ad664b2b4f087acb1a54afc1dc15ce8205cb3d8bf1472'
+                 'aaa765b1507b30934d4eb2047778e00')
+    data = {
+        'name': 'polyswarmtransaction.nectar:DepositTransaction',
+        'from': '0x3f17f1962B36e491b30A40b2405849e597Ba5FB5',
+        'data': {
+            'amount': '2000000000000000000'
+        }
+    }
+    transaction = DepositTransaction('2000000000000000000')
+    signed = transaction.sign(ethereum_accounts[0].key)
+    assert signed.raw_transaction == json.dumps(data)
+    assert signed.signature.hex() == signature
 
 
 def test_sign_withdrawal_transaction(ethereum_accounts):
@@ -37,9 +66,29 @@ def test_sign_withdrawal_transaction(ethereum_accounts):
     assert signed.signature.hex() == signature
 
 
+def test_recover_deposit_signed_transaction(ethereum_accounts):
+    transaction = DepositTransaction('2000000000000000000')
+    signed = transaction.sign(ethereum_accounts[0].key)
+    assert signed.ecrecover() == '0x3f17f1962B36e491b30A40b2405849e597Ba5FB5'
+
+
 def test_recover_withdrawal_signed_transaction(ethereum_accounts):
     transaction = WithdrawalTransaction('2000000000000000000')
     signed = transaction.sign(ethereum_accounts[0].key)
+    assert signed.ecrecover() == '0x3f17f1962B36e491b30A40b2405849e597Ba5FB5'
+
+
+def test_recover_deposit_signed_transaction_from_parts():
+    signature = ('0x59af24d65251f562dd8be5126917911aa1a660c8f5ce223f48e505ad664b2b4f087acb1a54afc1dc15ce8205cb3d8bf1472'
+                 'aaa765b1507b30934d4eb2047778e00')
+    data = {
+        'name': 'polyswarmtransaction.nectar:DepositTransaction',
+        'from': '0x3f17f1962B36e491b30A40b2405849e597Ba5FB5',
+        'data': {
+            'amount': '2000000000000000000'
+        }
+    }
+    signed = SignedTransaction(json.dumps(data), signature)
     assert signed.ecrecover() == '0x3f17f1962B36e491b30A40b2405849e597Ba5FB5'
 
 
@@ -57,11 +106,31 @@ def test_recover_withdrawal_signed_transaction_from_parts():
     assert signed.ecrecover() == '0x3f17f1962B36e491b30A40b2405849e597Ba5FB5'
 
 
+def test_recover_deposit_signed_transaction_from_signed_output(ethereum_accounts):
+    transaction = DepositTransaction('2000000000000000000')
+    signed = transaction.sign(ethereum_accounts[0].key)
+    signed = SignedTransaction(**signed.payload)
+    assert signed.ecrecover() == '0x3f17f1962B36e491b30A40b2405849e597Ba5FB5'
+
+
 def test_recover_withdrawal_signed_transaction_from_signed_output(ethereum_accounts):
     transaction = WithdrawalTransaction('2000000000000000000')
     signed = transaction.sign(ethereum_accounts[0].key)
     signed = SignedTransaction(**signed.payload)
     assert signed.ecrecover() == '0x3f17f1962B36e491b30A40b2405849e597Ba5FB5'
+
+
+def test_load_deposit():
+    data = {
+        'name': 'polyswarmtransaction.nectar:DepositTransaction',
+        'from': '0x3f17f1962B36e491b30A40b2405849e597Ba5FB5',
+        'data': {
+            'amount': '200000000000000000'
+        }
+    }
+    signed = SignedTransaction(json.dumps(data), bytes([0] * 65))
+    assert isinstance(signed.transaction(), DepositTransaction)
+    assert not DeepDiff(signed.transaction().data, DepositTransaction('200000000000000000').data, ignore_order=True)
 
 
 def test_load_withdrawal():
